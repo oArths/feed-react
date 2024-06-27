0f789786ec367d646ce8b593ec923e3b53f66e86
Arthur <arthurferreira1417@gmail.com>
2024-06-27 19:01:58 -0300

    Faet: criando navegação por swich

--- feed/src/index.js
+++ feed/src/index.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
+import Cadastro from './pages/cadastro';
 import Login from './pages/login';
+import Home from './pages/home';
 import './index.css';
+import { TokenProvider } from './context/TokenContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+function renderPage (){
+  const path = window.location.pathname
+
+  switch (path) {
+    case '/':
+      return <Login/>
+    case '/cadastro':
+      return <Cadastro/>
+    case '/home':
+      return <Home/>
+    default:
+      return <Login/>
+  }
+}
 root.render(
-  <React.StrictMode>
-    <Login />
+  <TokenProvider>
+<React.StrictMode>
+    {renderPage()}
   </React.StrictMode>
+  </TokenProvider>
+  
 );
 
 reportWebVitals();
