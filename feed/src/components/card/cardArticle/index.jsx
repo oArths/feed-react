43648ff4e89bccbf8bcb1c054b8bf1d5a5217c9c
Arthur <arthurferreira1417@gmail.com>
2024-06-27 18:59:41 -0300

    Feat: criando o componente card

---
+++ feed/src/components/card/cardArticle/index.jsx
@@ -0,0 +1,44 @@
+import "./style.css"
+import More from "../../../assests/imgs/more-vert.svg"
+import Comments from "../../../assests/imgs/comments.svg"
+
+
+export default function CardAricle({Description,Title, User}) {
+
+    const truncateText = (text, maxLength, ) => {
+        if (!text) return 'undefind key'; 
+        if (text.length > maxLength) {
+          return text.substring(0, maxLength) + '...';
+        }
+        return text;
+      };
+    return (
+        <div className="card">
+            <div className="header">
+                <div className="user">{truncateText(User, 20)}</div>
+                <div className="Option">
+                    <img className="more" src={More} />
+                </div>
+            </div>
+            <div className="title">
+                <div className="titleText">
+                    {truncateText(Title, 120)}
+                </div>
+            </div>
+            <div className="image">
+                <img className="ImgTag" src="https://www.macetesdemae.com/wp-content/uploads/2014/01/Auau.jpg" />
+            </div>
+            <div className="description">
+                <div className="descriptionText">
+                    {truncateText(Description, 250)}
+                </div>
+            </div>
+            <div className="comentarios">
+                <div className="commetsContainer">
+                <img className="icon" src={Comments}/>
+                </div>
+                
+            </div>
+        </div>
+    )
+}
