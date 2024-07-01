99583440c96e973da35f44652221c13f7ab5c3bd
Arthur <arthurferreira1417@gmail.com>
2024-07-01 15:26:51 -0300

    Faet: implementando as mudançãs na home

--- feed/src/pages/home/index.jsx
+++ feed/src/pages/home/index.jsx
@@ -1,12 +1,16 @@
 import "./style.css"
 import CardAricle from "../../components/card/cardArticle"
 import Heart from "../../assests/imgs/heart.svg"
+import HeaderHome from "../../components/header/headerHome"
+
 export default function Home (){
     return (
         <div className="body">
+            <HeaderHome/>
             <div className="feed">
 
             <CardAricle 
+            IsUser={true}
             UserImage={Heart}
             User="hehehe"
             Description="Est qui aut harum est corrupti modi omnis. Blanditjhhhddddddddhhhiis occaecati rerum soluta et quos. Cupiditate nostrum placeat est ducimus iusto repudiandae. Iure nostrum explicabo tempore rerum tenetur aut.
