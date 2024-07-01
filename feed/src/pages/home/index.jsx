a5de266beed2f3b97cd7030b2c66320c3417b902
Arthur <arthurferreira1417@gmail.com>
2024-07-01 19:07:48 -0300

    Feat: formatando

--- feed/src/pages/home/index.jsx
+++ feed/src/pages/home/index.jsx
@@ -1,12 +1,21 @@
-import "./style.css"
+import { useState } from "react"
 import CardAricle from "../../components/card/cardArticle"
 import Heart from "../../assests/imgs/heart.svg"
 import HeaderHome from "../../components/header/headerHome"
+import DropDownHeader from "../../components/dropdown/dropdownHeader/index.jsx"
+import "./style.css"
 
 export default function Home (){
+    const [Open, setOpen] = useState(false) 
+    const Home = () =>{
+        window.location.href = '/home'
+    }
     return (
         <div className="body">
-            <HeaderHome/>
+            <HeaderHome 
+            onclickPerfil={() => setOpen(!Open)} 
+            onclickHome={Home}
+            />
             <div className="feed">
 
             <CardAricle 
@@ -30,12 +39,9 @@ export default function Home (){
             Description="Est qui aut harum est corrupti modi omnis. Blanditjhhhddddddddhhhiis occaecati rerum soluta et quos. Cupiditate nostrum placeat est ducimus iusto repudiandae. Iure nostrum explicabo tempore rerum tenetur aut.
 "
             Title="Est qui aut harum est corrupti modi omnis. Blanditjhhhhhhhhhhhhhhhhhhhhhhhhhhhhhddddddddddddddddddddddddddddddddddddhhhiis occaecati rerum soluta et quos. Cupiditate nostrum placeat est ducimus iusto repudiandae. Iure nostrum explicabo tempore rerum tenetur aut.
-"/>
-
-
-          
+"/>          
             </div>
-            
+            <DropDownHeader IsOpen={Open} Blur={() => setOpen(!Open)} />
         </div>
     )
 }
