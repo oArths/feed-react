660cd75de1e467060859e7479015aa16db9774c5
Arthur <arthurferreira1417@gmail.com>
2024-07-01 19:06:23 -0300

    Faet: criando um dropdown de usuario pra ver perfil e fazer logout

---
+++ feed/src/components/dropdown/dropdownHeader/index.jsx
@@ -0,0 +1,50 @@
+import "./style.css"
+import logout from "../../../assests/imgs/log-out.svg"
+import User from "../../../assests/imgs/user.svg"
+import { useToken } from "../../../context/UseToken"
+
+export default function DropDownHeader({ IsOpen, Blur, Userperfil, }) {
+    const [token, setToken] = useToken()
+
+
+    const UserLogout = () => {
+        console.log("token", token)
+        fetch('http://127.0.0.1:8000/api/auth/siguin/delete', {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            },
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Logout realizado com sucesso:', data);
+                setToken();
+                window.location.href = "/"
+            })
+            .catch((error) => {
+                console.error('Erro ao realizar logout:', error);
+            });
+
+
+    }
+
+
+    if (IsOpen) {
+        return (
+            <div className="blur" onClick={Blur}>
+                <div className="Container">
+                    <div className="ConatinerUser" onClick={Userperfil}>
+                        <img className="imgicon" src={User} />
+
+                        <div className="verPerfil">Ver Perfil</div>
+                    </div>
+                    <div className="ConatinerLogOut" onClick={UserLogout}>
+                        <img className="imgiconLogOut" src={logout} />
+                        <div className="LogOut">Sair</div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
