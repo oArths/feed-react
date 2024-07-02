768f2b9fa369e37db783df79f18550ed79cb1a3d
Arthur <arthurferreira1417@gmail.com>
2024-07-02 17:20:13 -0300

    Style: criando a apgina de comentarios e post

---
+++ feed/src/pages/articlePage/index.jsx
@@ -0,0 +1,20 @@
+import styles from "./style.module.css"
+import CardAricle from "../../components/card/cardArticle"
+import HeaderArticleComments from "../../components/header/headerArticleComments"
+import ReplaceComments from "../../components/ReplaceComments"
+export default function ArticleComments() {
+    return (
+        <div className={styles.body} >
+            <HeaderArticleComments onclickBack={() => (window.location.href = '/home')}/>
+            <div className={styles.feed} >
+            <CardAricle 
+            IsUser={true}
+            // UserImage={}
+            User="hehehe"
+            Title="Est qui aut harum est corrupti modi omnis. Blanditjhhhhhhhhhhhhhhhhhhhhhhhhhhhhhddddddddddddddddddddddddddddddddddddhhhiis occaecati rerum soluta et quos. Cupiditate nostrum placeat est ducimus iusto repudiandae. Iure nostrum explicabo tempore rerum tenetur aut.
+"/>
+<ReplaceComments/>
+            </div>
+        </div>
+    )
+}
