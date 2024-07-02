6cf7fb9a716549ba5d188aba04eaf693343b2ce5
Arthur <arthurferreira1417@gmail.com>
2024-07-02 13:18:11 -0300

    Faet: ajustando as imagens pra serem responsivas[

--- feed/src/components/modalPost/index.jsx
+++ feed/src/components/modalPost/index.jsx
@@ -2,12 +2,18 @@ import styles from "./style.module.css"
 import Close from "../../assests/imgs/xmark.svg"
 import Folder from "../../assests/imgs/folder-plus.svg"
 import Empty from "../../assests/imgs/empty-page.svg"
+import Photo from "../../assests/imgs/media-image-list.svg"
+import Hashtag from "../../assests/imgs/hashtag.svg"
 import Trash from "../../assests/imgs/trash-solid.svg"
+
 
 
 import { useState } from "react"
 
 export default function ModalPost({ IsOpen, CloseModal, Subbmit, UserImage, User }) {
+
+    const [image, setImage] = useState(null)
+
     const truncateText = (text, maxLength,) => {
         if (!text) return 'undefind key';
         if (text.length > maxLength) {
@@ -15,19 +21,21 @@ export default function ModalPost({ IsOpen, CloseModal, Subbmit, UserImage, User
         }
         return text;
     };
+    const openFileSelector = () => {
+        document.querySelector(".input-files").click();
+    };
 
-    const [image, setImage] = useState(null)
-    const [fileName, setFileName] = useState("Sem Imagens Selecionadas")
+    const closeImage = () => {
+        setImage(null);
+    };
+
 
     if (IsOpen) {
         return (
-            <div className={styles.blur}>
-                <div className={styles.Conatiner}>
+            <div className={styles.blur} onClick={CloseModal}>
+                    <div className={styles.Conatiner} onClick={(e) => { e.stopPropagation();}}>
                     <div className={styles.header}>
                         <div>Criar Publicação</div>
-                        <div className={styles.imageContainer}>
-                            <img className={styles.img} src={Close} />
-                        </div>
                     </div>
                     <div className={styles.userConatiner}>
                         <div className={styles.UserPhoto}>
@@ -38,33 +46,36 @@ export default function ModalPost({ IsOpen, CloseModal, Subbmit, UserImage, User
                     <div className={styles.title}>
                         <textarea className={styles.input} type="text" maxLength={200} placeholder="O que você esta pensando?" />
                     </div>
-                    <div className={styles.Image} onClick={() => document.querySelector(".input-files").click()}>
+                    {!image ? 
+                    (<>
                         <input type="file"
                             accept="image/*"
                             className="input-files"
                             hidden
                             onChange={({ target: { files } }) => {
-                                files[0] && setFileName(files[0].name)
+
                                 if (files) {
                                     setImage(URL.createObjectURL(files[0]))
+                                } else {
+                                    setImage(null)
                                 }
                             }}
                         />
-                        {image ?
-                            <div className={styles.ContainerImg}>
-                                <img src={Close} onClick={() => { setImage(null) }} />
-                                <img src={image} width={350} height={350} />
-                            </div>
-                            :
-                            <div className={styles.ConatinerImage}>
-                                <img className={styles.FolderIcon} src={Empty} />
-                            </div>}
-                    </div>
-                    <div className="Description">
-                        <textarea className={styles.input} type="text" maxLength={200} placeholder="O que é sua deacrição" />
-                    </div>
-                    <div className="ConatinerButton">
-                        <button onClick={Subbmit}>Enviar</button>
+                       <div className={styles.Option}>
+                       <div className={styles.ConatinerImage} onClick={openFileSelector}>
+                            <img className={styles.FolderIcon} src={Photo} />
+                        </div>
+                        <div className={styles.ConatinerImage} onClick={openFileSelector}>
+                            <img className={styles.FolderIcon} src={Hashtag} />
+                        </div>
+                       </div>
+                    </>
+                    ) : (<div className={styles.ImageRender}>
+                                <img className={styles.Close} src={Close} onClick={closeImage} />
+                                <img src={image} className={styles.imageFull} />
+                    </div>)}
+                    <div className={styles.ConatinerButton}>
+                        <button onClick={Subbmit}>Publicar</button>
                     </div>
                 </div>
             </div>
