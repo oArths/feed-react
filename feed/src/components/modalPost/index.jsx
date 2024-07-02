087f5c9a6e71116fba6ea4d89ac7d370161db1eb
Arthur <arthurferreira1417@gmail.com>
2024-07-01 21:46:13 -0300

    Feat: update

--- feed/src/components/modalPost/index.jsx
+++ feed/src/components/modalPost/index.jsx
@@ -7,68 +7,67 @@ import Trash from "../../assests/imgs/trash-solid.svg"
 
 import { useState } from "react"
 
-    export default function ModalPost ({IsOpen, CloseModal, Subbmit}) {
-        const truncateText = (text, maxLength,) => {
-            if (!text) return 'undefind key';
-            if (text.length > maxLength) {
-                return text.substring(0, maxLength) + '...';
-            }
-            return text;
-        };
+export default function ModalPost({ IsOpen, CloseModal, Subbmit, UserImage, User }) {
+    const truncateText = (text, maxLength,) => {
+        if (!text) return 'undefind key';
+        if (text.length > maxLength) {
+            return text.substring(0, maxLength) + '...';
+        }
+        return text;
+    };
 
-        const [image, setImage] = useState(null)
-        const [fileName, setFileName] = useState("Sem Imagens Selecionadas")
+    const [image, setImage] = useState(null)
+    const [fileName, setFileName] = useState("Sem Imagens Selecionadas")
 
-       if(IsOpen){
+    if (IsOpen) {
         return (
-           <div className={styles.blur}>
-             <div className={styles.Conatiner}>
-                <div className={styles.header}>
-                    <div>Criar Publicação</div>
-                    <div className={styles.imageContainer}>
-                        <img className={styles.img} src={Close}  />
+            <div className={styles.blur}>
+                <div className={styles.Conatiner}>
+                    <div className={styles.header}>
+                        <div>Criar Publicação</div>
+                        <div className={styles.imageContainer}>
+                            <img className={styles.img} src={Close} />
+                        </div>
                     </div>
-                </div>
-                <div className={styles.title}>
-                    <textarea  className={styles.input} type="text" maxLength={200} placeholder="Digite seu titulo " />
-                </div>
-                <div className={styles.Image} onClick={() => document.querySelector(".input-files").click()}>
-                    <input type="file" 
-                    accept="image/*" 
-                    className="input-files" 
-                    hidden
-                    onChange={({target : {files}}) => {
-                        files[0] && setFileName(files[0].name)
-                        if(files){
-                            setImage(URL.createObjectURL(files[0]))
-                        }
-                    }}
-                    />
-                    {image ? 
-                    <img src={image} width={200} height={200} /> 
-                    : 
-                    <div className={styles.ConatinerImage}>
-                    <img className={styles.FolderIcon} src={Folder}/>
-                    </div>}
-                </div>
-                    <div className={styles.NameFile}>
-                    <img  src={Empty}/>
-                        {truncateText(fileName, 25)}
-                        <img  src={Trash}
-                        onClick={() => {
-                            setFileName("Nenhuma imagem selcionada")
-                            setImage(null)
-                        }}/>
-
+                    <div className={styles.userConatiner}>
+                        <div className={styles.UserPhoto}>
+                            <img src={UserImage} />
+                        </div>
+                        <div className="UserName">{truncateText(User, 20)}</div>
                     </div>
-                <div className="Description">
-                    <input type="text" />
-                </div>
-                <div className="ConatinerButton">
-                    <button onClick={Subbmit}>Enviar</button>
+                    <div className={styles.title}>
+                        <textarea className={styles.input} type="text" maxLength={200} placeholder="O que você esta pensando?" />
+                    </div>
+                    <div className={styles.Image} onClick={() => document.querySelector(".input-files").click()}>
+                        <input type="file"
+                            accept="image/*"
+                            className="input-files"
+                            hidden
+                            onChange={({ target: { files } }) => {
+                                files[0] && setFileName(files[0].name)
+                                if (files) {
+                                    setImage(URL.createObjectURL(files[0]))
+                                }
+                            }}
+                        />
+                        {image ?
+                            <div className={styles.ContainerImg}>
+                                <img src={Close} onClick={() => { setImage(null) }} />
+                                <img src={image} width={350} height={350} />
+                            </div>
+                            :
+                            <div className={styles.ConatinerImage}>
+                                <img className={styles.FolderIcon} src={Empty} />
+                            </div>}
+                    </div>
+                    <div className="Description">
+                        <textarea className={styles.input} type="text" maxLength={200} placeholder="O que é sua deacrição" />
+                    </div>
+                    <div className="ConatinerButton">
+                        <button onClick={Subbmit}>Enviar</button>
+                    </div>
                 </div>
             </div>
-           </div>
         )
-       }
     }
+}
