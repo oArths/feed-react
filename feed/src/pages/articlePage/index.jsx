f45ec74e3fb22039ffe4a690150650157d5b0983
Arthur <arthurferreira1417@gmail.com>
2024-07-02 19:10:47 -0300

    Feat: adicionando os comentarios na pagina

--- feed/src/pages/articlePage/index.jsx
+++ feed/src/pages/articlePage/index.jsx
@@ -5,15 +5,21 @@ import ReplaceComments from "../../components/ReplaceComments"
 export default function ArticleComments() {
     return (
         <div className={styles.body} >
-            <HeaderArticleComments onclickBack={() => (window.location.href = '/home')}/>
+            <HeaderArticleComments onclickBack={() => (window.location.href = '/home')} />
             <div className={styles.feed} >
-            <CardAricle 
-            IsUser={true}
-            // UserImage={}
-            User="hehehe"
-            Title="Est qui aut harum est corrupti modi omnis. Blanditjhhhhhhhhhhhhhhhhhhhhhhhhhhhhhddddddddddddddddddddddddddddddddddddhhhiis occaecati rerum soluta et quos. Cupiditate nostrum placeat est ducimus iusto repudiandae. Iure nostrum explicabo tempore rerum tenetur aut.
-"/>
-<ReplaceComments/>
+                <CardAricle
+                    IsUser={true}
+                    // UserImage={}
+                    User="hehehe"
+                    Title="Est qui aut harum est corrupti modi omnis. occaecati rerum soluta et quos. Cupiditate nostrum placeat est ducimus iusto repudiandae. Iure nostrum explicabo tempore rerum tenetur aut."/>
+                <div className={styles.Header}>
+                    <div className={styles.Title}>
+                        Respostas
+                    </div>
+                </div>
+                <ReplaceComments />
+                <ReplaceComments />
+                <ReplaceComments />
             </div>
         </div>
     )
