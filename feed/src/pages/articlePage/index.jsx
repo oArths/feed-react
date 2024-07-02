0189296e22649635be60c0d004829a2d7ca289fc
Arthur <arthurferreira1417@gmail.com>
2024-07-02 20:52:56 -0300

    Feat: passando pros componentes os dados pra renderizar

--- feed/src/pages/articlePage/index.jsx
+++ feed/src/pages/articlePage/index.jsx
@@ -1,14 +1,35 @@
+import { useState, useEffect } from "react"
 import styles from "./style.module.css"
 import CardAricle from "../../components/card/cardArticle"
 import HeaderArticleComments from "../../components/header/headerArticleComments"
 import ReplaceComments from "../../components/ReplaceComments"
+import ModalComment from "../../components/ModalComment/index.jsx";
+import ModalDeltePost from "../../components/modals/modalDeletePost/index.jsx"
+
+
 export default function ArticleComments() {
+    const [CreateOpen, setCreateOpen] = useState(false) 
+    const [ModalOpen, setOpenModal] = useState(false);
+    const [image, setImage] = useState(null)
+    const [ reply, setReplay] = useState({
+        user: 'texte',
+        title: 'texte',
+    })
+
+    const NewReply = (Info) =>{
+        setReplay(Info)
+        setCreateOpen(!CreateOpen)
+        // console.log(reply)
+    } 
     return (
         <div className={styles.body} >
             <HeaderArticleComments onclickBack={() => (window.location.href = '/home')} />
             <div className={styles.feed} >
                 <CardAricle
-                    IsUser={true}
+                onclickComments={() => NewReply({
+                    user: 'dsds',
+                    title: 'dsdssssss',
+                })}
                     // UserImage={}
                     User="hehehe"
                     Title="Est qui aut harum est corrupti modi omnis. occaecati rerum soluta et quos. Cupiditate nostrum placeat est ducimus iusto repudiandae. Iure nostrum explicabo tempore rerum tenetur aut."/>
@@ -17,10 +38,37 @@ export default function ArticleComments() {
                         Respostas
                     </div>
                 </div>
-                <ReplaceComments />
-                <ReplaceComments />
-                <ReplaceComments />
+                <ReplaceComments onclickComments={() => NewReply({
+                    user: 'comments',
+                    title: 'commentsss',
+                })} />
+                <ReplaceComments onclickComments={() => NewReply({
+                    user: 'comments',
+                    title: 'commentsss',
+                })} />
+                <ReplaceComments onclickComments={() => NewReply({
+                    user: 'comments',
+                    title: 'commentsss',
+                })} />
             </div>
+            <ModalComment 
+            UserAwnser={reply.user}
+            UserTextAwnser={reply.title}
+            User={reply.user}
+            IsOpen={CreateOpen} 
+            Subbmit={() => setCreateOpen(!CreateOpen)} 
+            CloseModal={() => setOpenModal(true)}  
+            setImage={setImage} image={image}
+            
+            />
+            <ModalDeltePost 
+            title="Descartar Comentario?" 
+            Confirm="Não"
+            onClickConfirm={() => setOpenModal(!ModalOpen)}
+            Delete="Descartar"
+            onClickDelete={() =>(setCreateOpen(!CreateOpen), setOpenModal(!ModalOpen), setImage(null)) }
+            isOpen={ModalOpen}
+            /> 
         </div>
     )
 }
