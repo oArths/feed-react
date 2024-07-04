f05f40a51f89269d45e7336c6695dbfe175c11e4
Arthur <arthurferreira1417@gmail.com>
2024-07-03 21:43:11 -0300

    Faet: userProfile

--- feed/src/components/userProfile/index.jsx
+++ feed/src/components/userProfile/index.jsx
@@ -1,12 +1,15 @@
 import styles from "./style.module.css"
 
 
-export default function UserProfile({ UserImage, User, onClickEditProfile }) {
+export default function UserProfile({ UserImage, User, UserBio,onClickEditProfile }) {
 
     return (
         <div className={styles.card}>
             <div className={styles.userConatiner}>
+                <div className={styles.UserInfo}>
                 <div className={styles.UserName}>{User}</div>
+                <div className={styles.UserBio}>{UserBio}</div>
+                </div>
                 <div className={styles.UserPhoto}>
                     <img className={styles.ImgeUser} src={UserImage} />
                 </div>
