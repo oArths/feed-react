0b8958007742d9ec727f670a38d9bbc2f4aeb559
Arthur <arthurferreira1417@gmail.com>
2024-07-04 14:45:07 -0300

    Fix: ajudatndo efeito do blur do dropdown delete

--- feed/src/components/dropdown/dropDownOption/index.jsx
+++ feed/src/components/dropdown/dropDownOption/index.jsx
@@ -1,6 +1,4 @@
 import styles from "./style.module.css"
-import Edit from "../../../assests/imgs/edit-pencil.svg"
-import Trash from "../../../assests/imgs/trash-solid.svg"
 import { useToken } from "../../../context/UseToken"
 
 export default function DropDownOptions({ IsOpen, Blur, UserEdit }) {
@@ -32,18 +30,16 @@ export default function DropDownOptions({ IsOpen, Blur, UserEdit }) {
 
     if (IsOpen) {
         return (
-            <div className={styles.blur} onClick={Blur}>
+            // <div className={styles.blur} onClick={Blur}>
                 <div className={styles.Container}>
                     <div className={styles.ConatinerUser} onClick={UserEdit}>
-                        <img className={styles.imgicon} src={Edit} />
                         <div className={styles.verPerfil}>Editar</div>
                     </div>
                     <div className={styles.ConatinerLogOut} onClick={UserDelete}>
-                        <img className={styles.imgiconLogOut} src={Trash} />
                         <div className={styles.LogOut}>Deletar</div>
                     </div>
                 </div>
-            </div>
+            // </div>
         )
     }
 }
