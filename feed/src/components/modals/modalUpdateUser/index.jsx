cfe91e2ecea2281b8e9ba00ca9d72932c2773f77
Arthur <arthurferreira1417@gmail.com>
2024-07-11 16:29:34 -0300

    Feat: limpando props

--- feed/src/components/modals/modalUpdateUser/index.jsx
+++ feed/src/components/modals/modalUpdateUser/index.jsx
@@ -1,6 +1,6 @@
 import styles from "./style.module.css"
 import { useState } from "react";
-export default function ModalUpdateuser({ isOpen, onClickBlur, setImage, image, onClickUpdateUser }) {
+export default function ModalUpdateuser({ isOpen, onClickBlur, setImage, image, onClickUpdateUser, UserValueName, UserValueDescription }) {
 
     const [error, setError] = useState({})
     const [values, setValues] = useState({
@@ -13,6 +13,7 @@ export default function ModalUpdateuser({ isOpen, onClickBlur, setImage, image, 
     const openFileSelector = () => {
         document.querySelector(".input-files").click();
     };
+    const baseURL = "http://127.0.0.1:8000/img/user/";
 
     if (isOpen) {
         return (
@@ -21,7 +22,7 @@ export default function ModalUpdateuser({ isOpen, onClickBlur, setImage, image, 
                 <div className={styles.Container} onClick={(e) => { e.stopPropagation() }}>
                    <div className={styles.ConatinerImage}>
                    <div className={styles.UserPhoto} onClick={openFileSelector}>
-                        <img className={styles.ImgeUser} src={image} />
+                        <img className={styles.ImgeUser} src={baseURL + image} />
                     </div>
                    </div>
                     <input type="file"
@@ -41,13 +42,13 @@ export default function ModalUpdateuser({ isOpen, onClickBlur, setImage, image, 
 
                         <div className={styles.InputConatiner}>
                             <div className={styles.Title}>Nome</div>
-                            <input type='text' onChange={(e) => setValues({ ...values, email: e.target.value })} value="Arths" placeholder='E-mail'className={styles.inputLogin} />
+                            <input type='text' onChange={(e) => setValues({ ...values, email: e.target.value })} value={UserValueName} placeholder='Nome'className={styles.inputLogin} />
                             <div className={styles.error}>{error && <div>{error.email}</div>}</div>
                         </div>
 
                         <div className={styles.InputConatiner}>
                             <div className={styles.Title}>Bio</div>
-                            <input type='text' onChange={(e) => setValues({ ...values, email: e.target.value })} maxLength={150} value="Uma breve descrição de voçê" placeholder='E-mail' className={styles.inputLogin} />
+                            <input type='text' onChange={(e) => setValues({ ...values, email: e.target.value })} maxLength={150} value={UserValueDescription} placeholder='E-mail' className={styles.inputLogin} />
                             <div className={styles.error}>{error && <div>{error.email}</div>}</div>
                         </div>
 
