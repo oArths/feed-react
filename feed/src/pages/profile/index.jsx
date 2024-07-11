9ab585d025f95b643883ac68b9ca5447f193306e
Arthur <arthurferreira1417@gmail.com>
2024-07-11 18:39:02 -0300

    Feat: redisytibuindo fuções  de chamada de modais

--- feed/src/pages/profile/index.jsx
+++ feed/src/pages/profile/index.jsx
@@ -3,19 +3,18 @@ import HeaderArticleComments from "../../components/header/headerArticleComments
 import UserProfile from "../../components/userProfile"
 import ModalUpdateuser from "../../components/modals/modalUpdateUser"
 import CardAricle from "../../components/card/cardArticle"
-import ModalPostEdit from "../../components/modalPostEdit/index"
 import { useState, useEffect } from "react"
 import { useToken } from "../../context/UseToken"
 
 export default function Profile() {
     const [ModalUpdateOpen, setModalUpdateOpen] = useState(false)
     const [ModalEdit, setModalEdit] = useState(false)
-    const [ModalEditPost, setModalEditPost] = useState(false)
     const [imageEdit, setImageEdit] = useState(null)
     const [token, setToken, UserId, setUserId, userData, setUserData, modify, setModify] = useToken()
-    const [image, setImage] = useState(userData[7])
+    
     const [Article, setArticle] = useState([])
     const [like, setLike] = useState(false)
+        const [image, setImage] = useState(userData[7])
 
     const baseURL = "http://127.0.0.1:8000/img/user/";
 
@@ -96,6 +95,7 @@ export default function Profile() {
                     {Article.map((Article, index) => (
                         <div key={index} >
                             <CardAricle
+                                PostImage={baseURL + Article.image}
                                 IsUser={true}
                                 PostIndex={index}
                                 UserImage={baseURL + userData[7]}
@@ -107,23 +107,15 @@ export default function Profile() {
                                 onclickComments={() => (window.location.href = '/home/article')}
                                 User={userData[1]}
                                 Title={Article.title} 
+                                
                                 PostId={Article.id}
                                 />
-
-                    
                         </div>
                     ))}
 
                 </div>
             </div>
-            <ModalPostEdit
-                Title="Editar publicação"
-                // Subbmit={() => setModalEditPost(!ModalEditPost)} 
-                CloseModal={() => setModalEditPost(!ModalEditPost)}
-                setImageEdit={setImageEdit}
-                imageEdit={imageEdit}
-                IsOpen={ModalEditPost}
-            />
+            
 
             <ModalUpdateuser
                 UserValueName={userData[1]}
