c213267d6cfbde193308321e1b50daf9a7a8c856
Arthur <arthurferreira1417@gmail.com>
2024-07-12 12:22:15 -0300

    Faet: ajustando imagens

--- feed/src/components/card/cardArticle/index.jsx
+++ feed/src/components/card/cardArticle/index.jsx
@@ -9,30 +9,30 @@ import ModalPostEdit from "../../modalPostEdit/index"
 import { useState } from "react"
 
 
-export default function CardAricle({ Title, 
-    User, UserImage, IsUser, 
-    onclickComments ,HeartCount, CommentsCount, like, onclickHeart, image,disabled, PostIndex, PostId, PostImage }) {
-        const [ModalEditPost, setModalEditPost] = useState(false)
-        const [ModalEdit, setModalEdit] = useState(false)
-        const [PostEdit, setPostEdit] = useState(PostIndex)
-        const [imagePost, setImage] = useState(PostImage)
-        const [ImageNull, setImageNull] = useState(false)
-        
-        const closeImage = () => {
-            setImage(null);
-            setImageNull(true);
-        };
+export default function CardAricle({ Title,
+    User, UserImage, IsUser,
+    onclickComments, HeartCount, CommentsCount, like, onclickHeart, image, disabled, PostIndex, PostId, PostImage }) {
+    const [ModalEditPost, setModalEditPost] = useState(false)
+    const [ModalEdit, setModalEdit] = useState(false)
+    const [PostEdit, setPostEdit] = useState(PostIndex)
+    const [imagePost, setImage] = useState(PostImage)
+    // const [ImageNull, setImageNull] = useState(false)
 
-        const UpdateUser = (index) => {
-            if(PostEdit === index){
-                setPostEdit(null)
-                setModalEdit(true)
-            }else{
-                setPostEdit(index)
-                setModalEdit(false)
-            }
-            // return console.log(index, PostEdit, ModalEdit);            
+    // const closeImage = () => {
+    //     setImage(null);
+    //     setImageNull(true);
+    // };
+
+    const UpdateUser = (index) => {
+        if (PostEdit === index) {
+            setPostEdit(null)
+            setModalEdit(true)
+        } else {
+            setPostEdit(index)
+            setModalEdit(false)
         }
+    }
+
     const truncateText = (text, maxLength,) => {
         if (!text) return 'undefind key';
         if (text.length > maxLength) {
@@ -40,28 +40,27 @@ export default function CardAricle({ Title,
         }
         return text;
     };
-    const baseURL = "http://127.0.0.1:8000/img/user/";
     return (
         <div className="card">
             <div className="header">
                 <div className="userConatiner">
                     <div className="UserPhoto">
-                        <img  className="ImgeUser" src={UserImage} />
+                        <img className="ImgeUser" src={UserImage} />
                     </div>
                     <div className="UserName">{truncateText(User, 20)}</div>
                 </div>
                 {IsUser &&
                     <>
-                    <div className="Option" onClick={() => UpdateUser(PostIndex)}>
-                        <img className="more" src={More} />
-                    </div>
-                     <DropDownOptions
-                     IsOpen={ModalEdit}
-                     id={PostId}
-                     UserEdit={() => (setModalEditPost(!ModalEditPost), setModalEdit(!ModalEdit))}
-                 />
+                        <div className="Option" onClick={() => UpdateUser(PostIndex)}>
+                            <img className="more" src={More} />
+                        </div>
+                        <DropDownOptions
+                            IsOpen={ModalEdit}
+                            id={PostId}
+                            UserEdit={() => (setModalEditPost(!ModalEditPost), setModalEdit(!ModalEdit))}
+                        />
                     </>
-                    }
+                }
             </div>
             <div className="title">
                 <div className="titleText">
@@ -69,32 +68,31 @@ export default function CardAricle({ Title,
                 </div>
             </div>
             {null !== (image) && (
-                
-            <div className="image">
-                <img className="ImgTag" src={baseURL + image}  />
-            </div>
+
+                <div className="image">
+                    <img className="ImgTag" src={image} />
+                </div>
             )}
             <div className="comentarios">
                 <div className="commetsContainer">
                     <div className="Houver" onClick={onclickHeart} disabled={disabled}>
-                        {like ? 
-                        (<img className="icon" src={HeartSolid}/>)
-                    :
-                    (<img className="icon" src={Heart}/>) }
+                        {like ?
+                            (<img className="icon" src={HeartSolid} />)
+                            :
+                            (<img className="icon" src={Heart} />)}
                         <div className="info">{HeartCount}</div>
                     </div>
                 </div>
                 <div className="commetsContainer">
                     <div className="Houver" onClick={onclickComments}>
-                        <img className="icon" src={Comments}  />
-                    <div className="info">{CommentsCount}</div>
+                        <img className="icon" src={Comments} />
+                        <div className="info">{CommentsCount}</div>
                     </div>
                 </div>
 
             </div>
             <ModalPostEdit
                 Title="Editar publicação"
-                // Subbmit={() => setModalEditPost(!ModalEditPost)} 
                 CloseModal={() => setModalEditPost(!ModalEditPost)}
                 setImageEdit={setImage}
                 imageEdit={imagePost}
@@ -102,9 +100,9 @@ export default function CardAricle({ Title,
                 Description={Title}
                 UserImage={UserImage}
                 User={User}
-            idPost={PostId}
-            ClearImage={ImageNull}
-            closeImage={closeImage}
+                idPost={PostId}
+            // ClearImage={ImageNull}
+            // closeImage={closeImage}
             />
         </div>
     )
