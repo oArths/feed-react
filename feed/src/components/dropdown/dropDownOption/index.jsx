d1a184d04e7351202e0b7dacbf3187d1a6f072ae
Arthur <arthurferreira1417@gmail.com>
2024-07-12 21:26:15 -0300

    Feat: movendo a função de deletar post pro pai

--- feed/src/components/dropdown/dropDownOption/index.jsx
+++ feed/src/components/dropdown/dropDownOption/index.jsx
@@ -1,30 +1,10 @@
 import styles from "./style.module.css"
-import { useToken } from "../../../context/UseToken"
-
-export default function DropDownOptions({ IsOpen, UserEdit, id }) {
-    const [token, setToken, UserId, setUserId, userData, setUserData, modify, setModify] = useToken()
 
 
-    const UserDelete = () => {
-        console.log("delete Post")
-        fetch(`http://127.0.0.1:8000/api/auth/articles/delete/${id}`, {
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`
-            },
-        })
-            .then(response => response.json())
-            .then(data => {
-                console.log('Post deletardo com sucesso:', data);
-                setModify(!modify)
-            })
-            .catch((error) => {
-                console.error('Erro ao apagar post', error);
-            });
+export default function DropDownOptions({ IsOpen, UserEdit, id, UserDelete}) {
 
 
-    }
+   
 
 
     if (IsOpen) {
