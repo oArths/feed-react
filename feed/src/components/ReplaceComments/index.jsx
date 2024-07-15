35557fe2cf7d975c50ed938c689a974af3dc33e2
Arthur <arthurferreira1417@gmail.com>
2024-07-15 20:57:50 -0300

    Faet: renderizadno caso estja curtido

--- feed/src/components/ReplaceComments/index.jsx
+++ feed/src/components/ReplaceComments/index.jsx
@@ -2,8 +2,10 @@ import styles from "./style.module.css"
 import Comments from "../../assests/imgs/comments.svg"
 import Heart from "../../assests/imgs/heart.svg"
 import UserDefault from "../../assests/imgs/userdefault.jpg"
+import HeartSolid from "../../assests/imgs/heart-solid.svg"
 
-export default function ReplaceComments({ UserImage, User, onclickHeart, onclickComments, Content, HeartCount, CommentsCount }) {
+export default function ReplaceComments({ like, UserImage, User, onclickHeart, onclickComments, Content, HeartCount, CommentsCount }) {
+    const baseURL = "http://127.0.0.1:8000/img/user/";
 
     const truncateText = (text, maxLength,) => {
         if (!text) return 'undefind key';
@@ -18,7 +20,7 @@ export default function ReplaceComments({ UserImage, User, onclickHeart, onclick
             <div className={styles.ConatinerCommentarios}>
                 <div className={styles.userConatiner}>
                     <div className={styles.UserPhoto}>
-                        <img className={styles.ImgeUser} src={UserImage === null ? UserDefault :UserImage} />
+                        <img className={styles.ImgeUser} src={UserImage === null ? UserDefault : baseURL + UserImage} />
                     </div>
                     <div className="UserName">{truncateText(User, 20)}</div>
                 </div>
@@ -28,7 +30,11 @@ export default function ReplaceComments({ UserImage, User, onclickHeart, onclick
                 <div className={styles.comentarios}>
                     <div className={styles.commetsContainer}>
                         <div className={styles.Houver} onClick={onclickHeart}>
-                            <img className={styles.icon} src={Heart}  />
+                            {like ? 
+                            (<img className={styles.icon} src={HeartSolid}  />)
+                            :
+                            (<img className={styles.icon} src={Heart}  />)}
+                            
                             <div className={styles.info}>{HeartCount}</div>
                         </div>
                     </div>
