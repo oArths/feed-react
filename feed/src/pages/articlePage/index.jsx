578d212048e1b2884d20d8e637451bda77f1c8a9
Arthur <arthurferreira1417@gmail.com>
2024-07-15 20:57:28 -0300

    Faet: consumindo os comentarios e fazendo as respostas deles

--- feed/src/pages/articlePage/index.jsx
+++ feed/src/pages/articlePage/index.jsx
@@ -13,12 +13,16 @@ export default function ArticleComments() {
     const [ModalOpen, setOpenModal] = useState(false);
     const [token, setToken, UserId, setUserId, userData, setUserData, modify, setModify] = useToken()
     const [image, setImage] = useState(null)
+    const [ImageNull, setImageNull] = useState(false)
     const [comments, setComments] = useState([])
     const [article, setArticle] = useState([])
     const [reply, setReplay] = useState({
         user: 'texte',
         title: 'texte',
+        UserImageAwmser: '',
+        commentsId: ''
     })
+
     const path = window.location.href
     const id = path.split('/')
 
@@ -42,13 +46,69 @@ export default function ArticleComments() {
             .then(data => {
                 setArticle(data.data.article[0])
                 setComments(data.data.comments)
-                console.log("data", data.data.comments)
-                console.log("data", data.data.article[0])
             })
             .catch(error => {
-                console.log("error", error)
+                window.location.href = '';
             })
-    }, [])
+    }, [modify])
+
+    const LikePost = (articleId, Liked) => {
+        const method = Liked ? 'DELETE' : 'POST';
+        fetch(`http://127.0.0.1:8000/api/auth/like/article`, {
+            method: method,
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`,
+            },
+            body: JSON.stringify({
+                'article_id': articleId,
+                'user_id': UserId
+            })
+        })
+            .then(response => {
+                return response.json()
+            })
+            .then(data => {
+                setModify(!modify)
+            })
+            .catch(error => {
+                window.location.href = '';
+            })
+
+    }
+
+    const likeComments = (ArticleId, CommentsId, Liked) => {
+        const method = Liked ? 'DELETE' : 'POST';
+
+        fetch('http://127.0.0.1:8000/api/auth/like/comment', {
+            method: method,
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            },
+            body: JSON.stringify({
+                "article_id": ArticleId,
+                "user_id": UserId,
+                "comment_id": CommentsId,
+            })
+        })
+            .then(response => {
+                if (!response.ok) {
+                    return response.json().then(errorData => {
+                        throw errorData
+                    })
+                }
+                return response.json()
+            }
+            )
+            .then(data => {
+                window.location.href = ''; 
+                setModify(!modify)
+            })
+            .catch(error => {
+                console.error(error)
+            })
+    }
 
     const NewReply = (Info) => {
         setReplay(Info)
@@ -67,6 +127,13 @@ export default function ArticleComments() {
                         like={article.liked_by_user}
                         HeartCount={article.likes_count}
                         CommentsCount={article.comments_count}
+                        onclickHeart={() => (LikePost(article.id, article.liked_by_user))}
+                        onclickComments={() => NewReply({
+                            UserImageAwmser: article.user.image,
+                            user: article.user.username,
+                            title: article.title,
+                        })}
+
                     />
                 )}
                 <div className={styles.Header}>
@@ -77,28 +144,39 @@ export default function ArticleComments() {
                 {comments.map((Comments, index) => (
                     <div key={index}>
 
-                        <ReplaceComments onclickComments={() => NewReply({
-                            user: 'comments',
-                            title: 'commentsss',
-                        })} 
-                        Content={Comments.content}
-                         HeartCount={Comments.likes_comments_count}
-                         CommentsCount={Comments.replies}
-                        User={Comments.user.username}
-                        UserImage={Comments.user.image}
+                        <ReplaceComments
+                            onclickComments={() => NewReply({
+                                user: Comments.user.username,
+                                title: Comments.content,
+                                UserImageAwmser: Comments.user.image,
+                                commentsId: Comments.id
+                            })}
+                            onclickHeart={() => likeComments(article.id, Comments.id, Comments.like_by_user_comment)}
+                            Content={Comments.content}
+                            like={Comments.like_by_user_comment}
+                            HeartCount={Comments.likes_comments_count}
+                            CommentsCount={Comments.replies.length}
+                            User={Comments.user.username}
+                            UserImage={Comments.user.image}
                         />
                     </div>
                 ))}
 
             </div>
             <ModalComment
+                UserImageAwmser={reply.UserImageAwmser}
                 UserAwnser={reply.user}
+                placeholderText={reply.user}
                 UserTextAwnser={reply.title}
-                User={reply.user}
+                User={userData[1]}
+                UserImage={userData[7]}
                 IsOpen={CreateOpen}
-                Subbmit={() => setCreateOpen(!CreateOpen)}
+                CloseFinaly={() => setCreateOpen(!CreateOpen)}
                 CloseModal={() => setOpenModal(true)}
                 setImage={setImage} image={image}
+                ClearImage={ImageNull}
+                articleId={article.id}
+                commentsId={reply.commentsId}
 
             />
             <ModalDeltePost
@@ -106,7 +184,7 @@ export default function ArticleComments() {
                 Confirm="Não"
                 onClickConfirm={() => setOpenModal(!ModalOpen)}
                 Delete="Descartar"
-                onClickDelete={() => (setCreateOpen(!CreateOpen), setOpenModal(!ModalOpen), setImage(null))}
+                onClickDelete={() => (setCreateOpen(!CreateOpen), setOpenModal(!ModalOpen), setImageNull(!ImageNull))}
                 isOpen={ModalOpen}
             />
         </div>
