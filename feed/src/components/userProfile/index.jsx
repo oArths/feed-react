8b97ffecaa05589c6f783c9f23799c64d34703d4
Arthur <arthurferreira1417@gmail.com>
2024-07-16 18:12:49 -0300

    Feat: ajustando styled do do card profile

--- feed/src/components/userProfile/index.jsx
+++ feed/src/components/userProfile/index.jsx
@@ -3,16 +3,22 @@ import UserDefault from "../../assests/imgs/userdefault.jpg"
 
 
 export default function UserProfile({ UserImage, User, UserBio,onClickEditProfile, disabled }) {
-
+    const truncateText = (text, maxLength,) => {
+        if (!text) return '';
+        if (text.length > maxLength) {
+            return text.substring(0, maxLength) + '...';
+        }
+        return text;
+    };
     return (
         <div className={styles.card}>
             <div className={styles.userConatiner}>
                 <div className={styles.UserInfo}>
-                <div className={styles.UserName}>{UserImage === null ? UserDefault :User}</div>
-                <div className={styles.UserBio}>{UserBio}</div>
+                <div className={styles.UserName}>{ truncateText(User, 20)}</div>
+                <div className={styles.UserBio}>{truncateText(UserBio, 100)}</div>
                 </div>
                 <div className={styles.UserPhoto}>
-                    <img className={styles.ImgeUser} src={UserImage} />
+                    <img className={styles.ImgeUser} src={UserImage === null ? UserDefault : UserImage} />
                 </div>
             </div>
             <div className={styles.ButtonConatiner}>
