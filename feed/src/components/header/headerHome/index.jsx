aa03323a90a20433f73fc609a9362948a8d52373
Arthur <arthurferreira1417@gmail.com>
2024-07-18 17:18:43 -0300

    Feat: refatorando o header

--- feed/src/components/header/headerHome/index.jsx
+++ feed/src/components/header/headerHome/index.jsx
@@ -1,4 +1,4 @@
-import "./style.css"
+import s from "./style.module.css"
 import home from "../../../assests/imgs/home-alt.svg"
 import plus from "../../../assests/imgs/plus-circle.svg"
 import UserDefault from "../../../assests/imgs/userdefault.jpg"
@@ -8,15 +8,15 @@ export default function HeaderHome({ UserPerfil, onclickPlus, onclickHome, oncli
     const baseURL = "http://127.0.0.1:8000/img/user/";
 
     return (
-        <div className="Conatiner">
-            <div className="Home" onClick={onclickHome}>
-                <img className="img" src={home} />
+        <div className={s.Conatiner}>
+            <div className={s.Home} onClick={onclickHome}>
+                <img className={s.img} src={home} />
             </div>
-            <div className="Home" onClick={onclickPlus}>
-                <img className="img" src={plus} />
+            <div className={s.Home} onClick={onclickPlus}>
+                <img className={s.img} src={plus} />
             </div>
-            <div className="perfil" onClick={onclickPerfil}>
-                <img  className="ImgeUser" src={UserPerfil === null ? UserDefault :baseURL +  UserPerfil} />
+            <div className={s.perfil} onClick={onclickPerfil}>
+                <img  className={s.ImgeUser} src={UserPerfil === null ? UserDefault :baseURL +  UserPerfil} />
             </div>
 
         </div>
